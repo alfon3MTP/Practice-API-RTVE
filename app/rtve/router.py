b09b9b99404163f3085a9171a9133bf24f5491a5
alfon3MTP <alfonso.tercero@mtp.es>
2024-11-11 08:22:40 +0100

    new endpoints

--- app/rtve/router.py
+++ app/rtve/router.py
@@ -1,11 +1,13 @@
 import random
 
+from app.rtve.schemas import ProgramTypeEnum, ProgramAge
 from fastapi import APIRouter, HTTPException, Path, Depends
 from typing import Annotated
 from sqlmodel import select, func, Session
 from app.rtve.models import TVProgram
 
 from app.db import get_session
+
 
 
 router = APIRouter(
@@ -46,12 +48,65 @@ async def get_random_program(
         ).select_from(TVProgram)
     )
     
-    print(num_progrms)
-    
-    rand_id = random.randint(1, 4668)
+    rand_id = random.randint(1, num_progrms.one())
     
     program = session.get(TVProgram, rand_id)
     
     if program is None:
         raise HTTPException(status_code=404, detail='Program not found')
     return program
+
+
+@router.get("/random_program_type", summary="Get a Random Program by Type")
+async def get_random_program_type(
+    program_type: ProgramTypeEnum = None,
+    age_range: ProgramAge = None,
+    session: Session = Depends(get_session)
+):
+
+    query = select(TVProgram)
+    
+    if program_type:
+        query = query.where(TVProgram.programType == program_type)
+    
+    print(age_range)
+    if age_range:
+        query = query.where(TVProgram.ageRangeUid == age_range)
+        
+    result = session.exec(query).all()
+    
+    if not result:
+        raise HTTPException(status_code=404, detail="No programs found")
+    
+    random_program = random.choice(result)
+    
+    return random_program
+
+
+@router.get("/program_by_showman", summary="Find Program by Showman")
+async def find_program_by_showman(
+    showman: str, 
+    session: Session = Depends(get_session)
+):
+    result = session.exec(
+        select(TVProgram).where(TVProgram.showMan.ilike(f"%{showman}%"))
+    ).all()
+
+    if not result:
+        raise HTTPException(status_code=404, detail="No programs found for the given showman")
+
+    return result
+
+@router.get("/program_by_name", summary="Find Program by Name")
+async def find_program_by_name(
+    name: str, 
+    session: Session = Depends(get_session)
+):
+    result = session.exec(
+        select(TVProgram).where(TVProgram.name.ilike(f"%{name}%"))
+    ).all()
+
+    if not result:
+        raise HTTPException(status_code=404, detail="No programs found with the given name")
+
+    return result
---
+++ app/rtve/schemas.py
@@ -0,0 +1,43 @@
+from enum import Enum
+
+class ProgramTypeEnum(str, Enum):
+    serie_documental = "Serie Documental"
+    infantiles = "Infantiles"
+    documental_original = "Documental Original"
+    magacines = "Magacines"
+    entrevistas = "Entrevistas"
+    magacin_semanal = "Magacín semanal"
+    informativos_noticias = "Informativos Noticias"
+    entretenimiento = "Entretenimiento"
+    concursos = "Concursos"
+    reportajes_factual = "Reportajes Factual"
+    series_ficcion_originales = "Series Ficción Originales"
+    magacines_diarios = "Magacines diarios"
+    talent = "Talent"
+    conciertos = "Conciertos"
+    series_internacionales = "Series Internacionales"
+    series = "Series"
+    series_prime_time = "Series Prime Time"
+    late_night = "Late Night"
+    especial_evento = "Especial Evento"
+    series_archivo = "Series de Archivo"
+    series_diarias = "Series Diarias"
+    recetas = "Recetas"
+    documental = "Documental"
+    series_catalan = "Series catalán"
+    contenedor_peliculas = "Contenedor Películas"
+    informativo_diario = "Informativo diario"
+    entretenimiento_no_usar = "Entretenimiento NO USAR"
+    
+
+class ProgramAge(str, Enum):
+    
+    IF_REDAD0 = 'Recomendado para todos los públicos'
+    IF_REDAD2 = 'Recomendado para mayores de 7 años'
+    IF_REDAD5 = 'Recomendado para mayores de 12 años'
+    IF_REDAD3 = 'Recomendado para mayores de 13 años'
+    IF_REDAD6 = 'Recomendado para mayores de 16 años'
+    IF_REDAD4 = 'Recomendado para mayores de 18 años'
+    IF_REDAD1 = 'Especialmente recomendado para la infancia'
+    
+    
